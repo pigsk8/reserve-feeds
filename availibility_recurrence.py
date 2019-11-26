04c2b17312d79c4cc0b1e35a9e735d484703f306
Leonard Mendoza <dranoel48@gmail.com>
2019-11-26 17:42:07 -0400

    fix merchants _sistema_id

--- availibility_recurrence.py
+++ availibility_recurrence.py
@@ -4,24 +4,29 @@ import json
 import time
 from datetime import datetime, timedelta
 
-with open('entry/sistema880.json') as file:
+with open('entry/sistema.json') as file:
     data = json.load(file)
 
 timestamp = int(time.time())
 
-#especificar fecha desde que se hara el recorrido
-fecha = '2019/11/27'
+# especificar fecha desde que se hara el recorrido
+fecha = '2019/11/26'
 current_day = datetime.strptime(fecha, '%Y/%m/%d')
+
+current_day_m = current_day.month
 current_day_d = current_day.day
+
+c_d = str(current_day.month)+'_'+str(current_day_d)
 
 array_week_dates = [current_day]
 
-#definir fecha a 30 dias
+# definir fecha a 30 dias
 for i in range(0, 30):
     array_week_dates.append(current_day + timedelta(days=i+1))
-    
+
 # list_lugares = [168, 226, 227, 228, 291, 349, 366, 603, 639, 683, 826, 843]
-list_lugares = [880]
+list_lugares = [168, 227]
+# list_lugares = [880]
 
 jsonAvailibility = {
     "metadata": {
@@ -49,101 +54,114 @@ for lugar in data:
 
                         # con recurrencia y excepciones
 
-                        dt_start = week_date + timedelta(seconds=day['start']*60)
-                        timestamp_start = (dt_start - datetime(1970, 1, 1)).total_seconds()
+                        dt_start = week_date + \
+                            timedelta(seconds=day['start']*60)
+                        timestamp_start = (
+                            dt_start - datetime(1970, 1, 1)).total_seconds()
 
                         dt_end = week_date + timedelta(seconds=(day['end'])*60)
-                        timestamp_end = (dt_end - datetime(1970, 1, 1)).total_seconds()
-                        
-                        dt_start2 = week_date + timedelta(seconds=day['start2']*60)
-                        timestamp_start2 = (dt_start2 - datetime(1970, 1, 1)).total_seconds()
+                        timestamp_end = (
+                            dt_end - datetime(1970, 1, 1)).total_seconds()
 
-                        dt_end2 = week_date + timedelta(seconds=(day['end2']-30)*60)
-                        timestamp_end2 = (dt_end2 - datetime(1970, 1, 1)).total_seconds()
+                        dt_start2 = week_date + \
+                            timedelta(seconds=day['start2']*60)
+                        timestamp_start2 = (
+                            dt_start2 - datetime(1970, 1, 1)).total_seconds()
 
-                        for party_size in range(1,11):
-                                jsonAvailibilityServA.append({
-                                    "duration_sec": 1800, 
-                                    "start_sec": int(timestamp_start),
-                                    "merchant_id": "merch"+str(lugar['id']),
-                                    "service_id": str(lugar['id'])+"-dining",
-                                    "spots_open": 10,
-                                    "spots_total": 10,
-                                    "ConfirmationMode": "CONFIRMATION_MODE_ASYNCHRONOUS",
-                                    "resources": {
-                                        "party_size": party_size
-                                    },
-                                    "recurrence": {
-                                        "repeat_until_sec": int(timestamp_end2),
-                                        "repeat_every_sec": 1800
-                                    },
-                                    "schedule_exception": [
-                                        {
-                                            "time_range": {
-                                                "begin_sec": int(timestamp_end),
-                                                "end_sec": int(timestamp_start2)
-                                            }
+                        dt_end2 = week_date + \
+                            timedelta(seconds=(day['end2']-30)*60)
+                        timestamp_end2 = (
+                            dt_end2 - datetime(1970, 1, 1)).total_seconds()
+
+                        for party_size in range(1, 11):
+                            jsonAvailibilityServA.append({
+                                "duration_sec": 1800,
+                                "start_sec": int(timestamp_start),
+                                "merchant_id": "merch"+str(lugar['id']),
+                                "service_id": str(lugar['id'])+"-dining",
+                                "spots_open": 10,
+                                "spots_total": 10,
+                                "ConfirmationMode": "CONFIRMATION_MODE_ASYNCHRONOUS",
+                                "resources": {
+                                    "party_size": party_size
+                                },
+                                "recurrence": {
+                                    "repeat_until_sec": int(timestamp_end2),
+                                    "repeat_every_sec": 1800
+                                },
+                                "schedule_exception": [
+                                    {
+                                        "time_range": {
+                                            "begin_sec": int(timestamp_end),
+                                            "end_sec": int(timestamp_start2)
                                         }
-                                    ]
-                                })
+                                    }
+                                ]
+                            })
 
-
-                    
                     else:
 
                         #######################################
                         # RECURRENCIA
                         if(day['start'] != 0 and day['end'] != 0):
 
-                            dt_start = week_date + timedelta(seconds=day['start']*60)
-                            timestamp_start = (dt_start - datetime(1970, 1, 1)).total_seconds()
+                            dt_start = week_date + \
+                                timedelta(seconds=day['start']*60)
+                            timestamp_start = (
+                                dt_start - datetime(1970, 1, 1)).total_seconds()
 
-                            dt_end = week_date + timedelta(seconds=(day['end']-30)*60)
-                            timestamp_end = (dt_end - datetime(1970, 1, 1)).total_seconds()
+                            dt_end = week_date + \
+                                timedelta(seconds=(day['end']-30)*60)
+                            timestamp_end = (
+                                dt_end - datetime(1970, 1, 1)).total_seconds()
 
-                            for party_size in range(1,11):
-                                    jsonAvailibilityServA.append({
-                                        "duration_sec": 1800, 
-                                        "start_sec": int(timestamp_start),
-                                        "merchant_id": "merch"+str(lugar['id']),
-                                        "service_id": str(lugar['id'])+"-dining",
-                                        "spots_open": 10,
-                                        "spots_total": 10,
-                                        "resources": {
-                                            "party_size": party_size
-                                        },
-                                        "recurrence": {
-                                            "repeat_until_sec": int(timestamp_end),
-                                            "repeat_every_sec": 1800
-                                        },
-                                        "confirmation_mode": "CONFIRMATION_MODE_ASYNCHRONOUS"
-                                    })
+                            for party_size in range(1, 11):
+                                jsonAvailibilityServA.append({
+                                    "duration_sec": 1800,
+                                    "start_sec": int(timestamp_start),
+                                    "merchant_id": "merch"+str(lugar['id']),
+                                    "service_id": str(lugar['id'])+"-dining",
+                                    "spots_open": 10,
+                                    "spots_total": 10,
+                                    "resources": {
+                                        "party_size": party_size
+                                    },
+                                    "recurrence": {
+                                        "repeat_until_sec": int(timestamp_end),
+                                        "repeat_every_sec": 1800
+                                    },
+                                    "confirmation_mode": "CONFIRMATION_MODE_ASYNCHRONOUS"
+                                })
 
                         if(day['start2'] != 0 and day['end2'] != 0):
 
-                            dt_start = week_date + timedelta(seconds=day['start2']*60)
-                            timestamp_start = (dt_start - datetime(1970, 1, 1)).total_seconds()
+                            dt_start = week_date + \
+                                timedelta(seconds=day['start2']*60)
+                            timestamp_start = (
+                                dt_start - datetime(1970, 1, 1)).total_seconds()
 
-                            dt_end = week_date + timedelta(seconds=(day['end2']-30)*60)
-                            timestamp_end = (dt_end - datetime(1970, 1, 1)).total_seconds()
+                            dt_end = week_date + \
+                                timedelta(seconds=(day['end2']-30)*60)
+                            timestamp_end = (
+                                dt_end - datetime(1970, 1, 1)).total_seconds()
 
-                            for party_size in range(1,11):
-                                    jsonAvailibilityServA.append({
-                                        "duration_sec": 1800, 
-                                        "start_sec": int(timestamp_start),
-                                        "merchant_id": "merch"+str(lugar['id']),
-                                        "service_id": str(lugar['id'])+"-dining",
-                                        "spots_open": 10,
-                                        "spots_total": 10,
-                                        "resources": {
-                                            "party_size": party_size
-                                        },
-                                        "recurrence": {
-                                            "repeat_until_sec": int(timestamp_end),
-                                            "repeat_every_sec": 1800
-                                        },
-                                        "confirmation_mode": "CONFIRMATION_MODE_ASYNCHRONOUS" 
-                                    })
+                            for party_size in range(1, 11):
+                                jsonAvailibilityServA.append({
+                                    "duration_sec": 1800,
+                                    "start_sec": int(timestamp_start),
+                                    "merchant_id": "merch"+str(lugar['id']),
+                                    "service_id": str(lugar['id'])+"-dining",
+                                    "spots_open": 10,
+                                    "spots_total": 10,
+                                    "resources": {
+                                        "party_size": party_size
+                                    },
+                                    "recurrence": {
+                                        "repeat_until_sec": int(timestamp_end),
+                                        "repeat_every_sec": 1800
+                                    },
+                                    "confirmation_mode": "CONFIRMATION_MODE_ASYNCHRONOUS"
+                                })
 
 
 jsonAvailibilityServ['availability'] = jsonAvailibilityServA
@@ -151,5 +169,5 @@ jsonAvailibilityServi.append(jsonAvailibilityServ)
 
 jsonAvailibility['service_availability'] = jsonAvailibilityServi
 
-with open('output/availabilityRecurrence'+str(current_day_d)+'.json', 'w') as file:
+with open('output/availabilityRecurrence'+str(c_d)+'.json', 'w') as file:
     json.dump(jsonAvailibility, file)
--- merchants.py
+++ merchants.py
@@ -5,16 +5,25 @@ import time
 from datetime import datetime, timedelta
 
 with open('entry/lugares.json') as file:
-	data_lugar = json.load(file)
+    data_lugar = json.load(file)
 
 with open('entry/metas.json') as fileM:
-	data_metas = json.load(fileM)
+    data_metas = json.load(fileM)
 
 timestamp = int(time.time())
 
+# especificar fecha desde que se hara el recorrido
+fecha = '2019/11/26'
+current_day = datetime.strptime(fecha, '%Y/%m/%d')
+
+current_day_m = current_day.month
+current_day_d = current_day.day
+
+c_d = str(current_day.month)+'_'+str(current_day_d)
+
 # Lugares a agregar IDs
-# Cantina La No. 20 Antara
-# Restaurante Rosa Negra Tulum
+# Cantina La No. 20 Antara post-1983 id-168
+# Restaurante Rosa Negra Tulum post-2546 id-227
 # Restaurante Mariachito Insurgentes
 # Restaurante Pubbelly Sushi Polanco
 # Restaurante Jaso Polanco
@@ -23,97 +32,99 @@ timestamp = int(time.time())
 # Restaurante STK México Polanco
 # Restaurante Rosa Negra Cancún
 # Restaurante Rosa Negra Polanco
-list_lugares = ['3480', '11317', '6079', '2947', '5198',
-    '6123', '2552', '2540', '2546', '3586', '11421', '1983']
+
+# list_lugares = ['3480', '11317', '6079', '2947', '5198',
+#     '6123', '2552', '2540', '2546', '3586', '11421', '1983']
+
+list_lugares = ['1983', '2546']
 
 jsonMerchant = {
-	"metadata": {
-		"generation_timestamp": timestamp,
-		"processing_instruction": "PROCESS_AS_INCREMENTAL",
-		"shard_number": 0,
-		"total_shards": 1
-	}
+    "metadata": {
+        "generation_timestamp": timestamp,
+        "processing_instruction": "PROCESS_AS_INCREMENTAL",
+        "shard_number": 0,
+        "total_shards": 1
+    }
 }
 jsonMerchantInfo = []
 
 
 jsonService = {
     "metadata": {
-            "generation_timestamp": timestamp,
-            "processing_instruction": "PROCESS_AS_INCREMENTAL",
-            "shard_number": 0,
-            "total_shards": 1
-	},
+        "generation_timestamp": timestamp,
+        "processing_instruction": "PROCESS_AS_INCREMENTAL",
+        "shard_number": 0,
+        "total_shards": 1
+    },
 }
 jsonServiceInfo = []
 
 for lugar in data_lugar:
+    
+    if lugar['ID'] in list_lugares:
+        name = lugar['post_title']
+        url = 'https://reservandonos.com/lugar/'+lugar['post_name']+'/'
 
-	if lugar['ID'] in list_lugares:
+        sistema_id = postal_code = 0
 
-		name = lugar['post_title']
-		url = 'https://reservandonos.com/lugar/'+lugar['post_name']+'/'
+        for meta in data_metas:
 
-		sistema_id = postal_code = 0
+            if meta['post_id'] == lugar['ID']:
 
-		for meta in data_metas:
+                if meta['meta_key'] == '_sistema_id':
+                    sistema_id = meta['meta_value']
 
-			if meta['post_id'] == lugar['ID']:
+                if meta['meta_key'] == 'geolocation_formatted_address':
+                    street_address = meta['meta_value']
 
-				if meta['meta_key'] == '_sistema_id':
-					sistema_id = meta['meta_value']
+                if meta['meta_key'] == 'geolocation_city':
+                    locality = meta['meta_value']
 
-				if meta['meta_key'] == 'geolocation_formatted_address':
-					street_address = meta['meta_value']
+                if meta['meta_key'] == 'geolocation_state_short':
+                    region = meta['meta_value']
 
-				if meta['meta_key'] == 'geolocation_city':
-					locality = meta['meta_value']
+                if meta['meta_key'] == 'geolocation_country_short':
+                    country = meta['meta_value']
 
-				if meta['meta_key'] == 'geolocation_state_short':
-					region = meta['meta_value']
+                if meta['meta_key'] == 'geolocation_postcode':
+                    postal_code = meta['meta_value']
 
-				if meta['meta_key'] == 'geolocation_country_short':
-					country = meta['meta_value']
+        if (sistema_id != 0):
 
-				if meta['meta_key'] == 'geolocation_postcode':
-					postal_code = meta['meta_value']
+            jsonServiceInfoAdd = [{
+                "url": url
+            }]
 
-		if (sistema_id == 0):
+            jsonServiceInfo.append({
+                "merchant_id": "merch"+str(sistema_id),
+                "service_id": str(sistema_id)+"-dining",
+                "name":  "Reservation",
+                "type":  "SERVICE_TYPE_DINING_RESERVATION",
+                "action_link": jsonServiceInfoAdd
+            })
 
-			jsonServiceInfoAdd = [{
-				"url": url
-			}]
-
-			jsonServiceInfo.append({
-				"merchant_id": "merch"+str(sistema_id),
-				"service_id": str(sistema_id)+"-dining",
-				"name":  "Reservation",
-				"type":  "SERVICE_TYPE_DINING_RESERVATION",
-				"action_link": jsonServiceInfoAdd
-			})
-
-			jsonMerchantInfoAdd = {
-				"address": {
+            jsonMerchantInfoAdd = {
+                "address": {
                     "street_address": street_address,
                     "locality": locality,
                     "region": region,
                     "country": country,
                     "postal_code": postal_code
-				}
-			}
+                }
+            }
 
-			jsonMerchantInfo.append({
-				"merchant_id": "merch"+str(sistema_id),
-				"name":  name,
-				"category": "restaurant",
-				"geo": jsonMerchantInfoAdd
-			})
+            jsonMerchantInfo.append({
+                "merchant_id": "merch"+str(sistema_id),
+                "name":  name,
+                "category": "restaurant",
+                "geo": jsonMerchantInfoAdd
+            })
 
 jsonMerchant['merchant'] = jsonMerchantInfo
 jsonService['service'] = jsonServiceInfo
 
-with open('output/merchants.json', 'w') as file:
+with open('output/merchants'+str(c_d)+'.json', 'w') as file:
     json.dump(jsonMerchant, file)
 
-with open('output/service.json', 'w') as file:
+with open('output/services'+str(c_d)+'.json', 'w') as file:
     json.dump(jsonService, file)
